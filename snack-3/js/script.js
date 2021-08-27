2369c19eb3b415d28e33f3fbbe0b65e24a1447a0
DavideBernardini <davidebernardini09@gmail.com>
2021-08-27 12:57:45 +0200

    Aggiunto esercizio

---
+++ snack-3/js/script.js
@@ -0,0 +1,13 @@
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+// lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+var nomi = ["Mario", "Luigi", "Claudio", "Franco", "Giuseppe"];
+var cognomi = ["Rossi", "Verdi", "Bianchi", "Neri", "Gialli"];
+var listaInvitati = []
+
+for (var i = 0; i < 3; i++) {
+   listaInvitati.push(nomi[Math.floor(Math.random() * nomi.length)] + " " + cognomi[Math.floor(Math.random() * cognomi.length)]);
+}
+
+console.log(listaInvitati)
+
